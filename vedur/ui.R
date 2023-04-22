3697f5865de1fc921b47119fe99d5a021b0579ee
arniEstima <arni@estima.is>
2023-04-22 16:38:03 +0000

    sdf

--- vedur/ui.R
+++ vedur/ui.R
@@ -8,33 +8,6 @@ unique_time_points <- unique(weatherDt$ftime)
 time_labels <- setNames(as.character(unique_time_points), as.character(unique_time_points))
 
 ui <- fluidPage(
-  tags$head(
-    tags$style(
-      HTML("
-      .center_plot {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-    ")
-    ),
-    tags$script(
-      HTML("
-      function setPlotHeight() {
-        var plotWrapper = document.getElementById('ice_plot');
-        var plotElement = plotWrapper.querySelector('.shiny-plot-output');
-        var aspectRatio = 0.6; // Replace with your desired aspect ratio
-        plotElement.style.height = (plotWrapper.offsetWidth * aspectRatio) + 'px';
-      }
-
-      // Set plot height on initial load
-      document.addEventListener('DOMContentLoaded', setPlotHeight);
-
-      // Update plot height on window resize
-      window.addEventListener('resize', setPlotHeight);
-    ")
-    )
-  ),
   titlePanel("Weather Data in Iceland"),
   fluidRow(
     div(
